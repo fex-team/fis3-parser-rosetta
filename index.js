ab989e22db45348ba52a24cb2aeef00ba279fc34
2betop <liaoxuezhi@icloud.com>
2015-07-16 14:41:49 +0800

    better error display

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ module.exports = function(content, file, opt) {
   try {
     content = transform(content, file, opt);
   } catch (e) {
-    fis.log.warn('Got error: %s while parsing `%s`.', e.message, file.subpath);
+    fis.log.warn('Got error: %s while parsing `%s`.%s', e.message.red, file.subpath, e.detail || '');
     fis.log.debug(e.stack);
   }
 
